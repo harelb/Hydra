9671329c027e7c2bf567d2f67549b0b2b6da36cf
harelb <harelb@mit.edu>
2026-07-02 17:23:05 -0400

    feat: extract reusable KeyframeGate helper with tests

---
+++ src/frontend/keyframe_gate.cpp
@@ -0,0 +1,28 @@
+#include "hydra/frontend/keyframe_gate.h"
+
+namespace hydra {
+
+bool KeyframeGate::shouldTrigger(const Eigen::Vector3d& position,
+                                 const Eigen::Quaterniond& orientation) {
+  bool trigger = false;
+  if (!initialized_) {
+    trigger = true;
+  } else {
+    const double translation_diff = (position - last_position_).norm();
+    const double angular_diff =
+        last_orientation_.angularDistance(orientation) * 180.0 / M_PI;
+    if (translation_diff >= config_.min_translation_m ||
+        angular_diff >= config_.min_rotation_deg) {
+      trigger = true;
+    }
+  }
+
+  if (trigger) {
+    last_position_ = position;
+    last_orientation_ = orientation;
+    initialized_ = true;
+  }
+  return trigger;
+}
+
+}  // namespace hydra
